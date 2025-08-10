e501b393211611096f471bc090197b0eb130ec7d
Apurvborhade <apurvaborhadee@gmail.com>
2025-08-10 20:13:48 +0530

    job scheduler

---
+++ backend/src/jobs/pinger.ts
@@ -0,0 +1,39 @@
+import cron from "node-cron";
+import fetch from 'node-fetch'
+import prisma from "../prisma";
+
+async function pingMonitors() {
+    console.log("Monitor Pinger Job Started 🟢")
+    const monitors = await prisma.monitor.findMany();
+
+    for (const monitor of monitors) {
+        const start = Date.now();
+
+        try {
+            const res = await fetch(monitor.url, { method: "GET" })
+            const latency = Date.now() - start;
+            await prisma.monitorHistory.create({
+                data: {
+                    monitorId: monitor.id,
+                    success: true,
+                    latencyMs: latency,
+                    statusCode: res.status
+                }
+            })
+            console.log(`✅ ${monitor.url} - ${res.status} (${latency}ms)`);
+        } catch (err: any) {
+            const latency = Date.now() - start;
+            await prisma.monitorHistory.create({
+                data: {
+                    monitorId: monitor.id,
+                    success: false,
+                    latencyMs: latency,
+                    error: err.message,
+                }
+            });
+            console.error(`❌ ${monitor.url} - ${err.message}`);
+        }
+    }
+}
+
+cron.schedule("* * * * *", pingMonitors);
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,13 +3,13 @@ const app: Application = express();
 
 import dotenv from 'dotenv'
 import errorHandler from './middleware/errorHandler';
-
+import './jobs/pinger'
 // ENVs
 dotenv.config()
 const PORT = process.env.PORT || 8000;
 
 
 app.use(errorHandler)
-app.listen(() => {
+app.listen(PORT, () => {
     console.log(`Server listening on PORT ${PORT}`)
 })
